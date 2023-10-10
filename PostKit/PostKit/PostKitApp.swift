eeebd00bb824617af007e346dbbdd5361b0e0167
김다빈 <mokdb17@gmail.com>
2023-10-10 19:31:20 +0900

    initial setting

---
+++ PostKit/PostKit/PostKitApp.swift
@@ -0,0 +1,20 @@
+//
+//  PostKitApp.swift
+//  PostKit
+//
+//  Created by 김다빈 on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PostKitApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
