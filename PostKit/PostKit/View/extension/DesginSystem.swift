91e8bd1a97c7a6fb907d91614e530eff9ea2152a
김다빈 <mokdb17@gmail.com>
2023-10-11 19:55:10 +0900

    디자인 시스템 패딩값 적용

---
+++ PostKit/PostKit/View/extension/DesginSystem.swift
@@ -0,0 +1,18 @@
+//
+//  DesginSystem.swift
+//  PostKit
+//
+//  Created by 김다빈 on 10/11/23.
+//
+
+import SwiftUI
+
+struct DesginSystem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DesginSystem()
+}
