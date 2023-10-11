3974491d5a20b58c81980c568b77843376bd9243
김다빈 <mokdb17@gmail.com>
2023-10-11 16:04:14 +0900

    파일 흐름관리 초기버전

---
+++ PostKit/PostKit/View/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  PostKit
+//
+//  Created by 김다빈 on 10/11/23.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
