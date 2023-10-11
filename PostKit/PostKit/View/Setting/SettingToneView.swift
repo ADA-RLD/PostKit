0108a4bae71ffa7a49098dc216f0043014a20ce3
김다빈 <mokdb17@gmail.com>
2023-10-11 17:15:17 +0900

    [Feat/#1] add MainSettingView

--- PostKit/PostKit/View/Setting/SettingToneView.swift
+++ PostKit/PostKit/View/Setting/SettingToneView.swift
@@ -1,5 +1,5 @@
 //
-//  SettingView.swift
+//  SettingToneView.swift
 //  PostKit
 //
 //  Created by 김다빈 on 10/11/23.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct SettingView: View {
+struct SettingToneView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    SettingView()
+    SettingToneView()
 }
