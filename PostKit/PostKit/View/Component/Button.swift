3c46d1fa0edaf9a4c1a74aa465bd2dba1cca9d40
김다빈 <mokdb17@gmail.com>
2023-10-11 09:18:58 +0900

    파일 폴더링

---
+++ PostKit/PostKit/View/Component/Button.swift
@@ -0,0 +1,18 @@
+//
+//  Button.swift
+//  PostKit
+//
+//  Created by 김다빈 on 10/11/23.
+//
+
+import SwiftUI
+
+struct Button: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Button()
+}
---
+++ PostKit/PostKit/ViewModel/ViewModelSample.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModelSample.swift
+//  PostKit
+//
+//  Created by 김다빈 on 10/11/23.
+//
+
+import Foundation
