951a308df331751a4b005256e52a186cc42e15fe
김다빈 <mokdb17@gmail.com>
2023-10-12 03:22:36 +0900

    [rash-feat 디자인 시스템 구현 #1]

--- PostKit/PostKit/View/Setting/SettingStoreView.swift
+++ PostKit/PostKit/View/Setting/SettingStoreView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SettingStoreView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.title1())
             
     }
 }
