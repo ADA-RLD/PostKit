068e725e7f9fd52fa4a6c4ffa9073181eb07993d
김다빈 <mokdb17@gmail.com>
2023-10-11 20:54:36 +0900

    [rash-Setting Add color Asset #1]

--- PostKit/PostKit/View/Component/CustomBtn.swift
+++ PostKit/PostKit/View/Component/CustomBtn.swift
@@ -1,5 +1,5 @@
 //
-//  Button.swift
+//  CustomBtn.swift
 //  PostKit
 //
 //  Created by 김다빈 on 10/11/23.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct Button: View {
+struct CustomBtn: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Button()
+    CustomBtn()
 }
