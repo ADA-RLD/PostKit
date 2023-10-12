a726ee36aae462b422fa474ac56085c1bc85b715
HIBAR97 <hibar9710@gmail.com>
2023-10-12 14:10:36 +0900

    [Data/#1] Daily, Menu Model 기록 ID, 날짜 변수 추가

--- PostKit/PostKit/Model/MenuModel.swift
+++ PostKit/PostKit/Model/MenuModel.swift
@@ -11,12 +11,16 @@ import Foundation
      let storeName : String?
      let storeTone : String
 
+     @Published var recordID = UUID()
+     @Published var recordDate : Date?
      @Published var menuName : String
      @Published var menuPoint : String
 
-     init(_storeName: String? = nil, _storeTone: String, _menuName: String, _menuPoint: String) {
+     init(_storeName: String?, _storeTone: String, _recordID: UUID = UUID(), _recordDate: Date? = nil, _menuName: String, _menuPoint: String) {
          self.storeName = _storeName
          self.storeTone = _storeTone
+         self.recordID = _recordID
+         self.recordDate = _recordDate
          self.menuName = _menuName
          self.menuPoint = _menuPoint
      }
