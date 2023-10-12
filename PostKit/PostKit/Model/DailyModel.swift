3e47573e48427479cf2fe970f4e40ec1df168c44
HIBAR97 <hibar9710@gmail.com>
2023-10-12 14:10:36 +0900

    [Data/#1] Daily, Menu Model 기록 ID, 날짜 변수 추가

--- PostKit/PostKit/Model/DailyModel.swift
+++ PostKit/PostKit/Model/DailyModel.swift
@@ -11,13 +11,17 @@ final class DailyModel : ObservableObject{
     let storeName : String?
     let storeTone : String
 
+    @Published var recordID = UUID()
+    @Published var recordDate : Date
     @Published var weather : String?
     @Published var dessert : String?
     @Published var drink : String?
 
-    init(_storeName: String? = nil, _storeTone: String, _weather: String? = nil, _dessert: String? = nil, _drink: String? = nil) {
+    init(_storeName: String?, _storeTone: String, _recordID: UUID = UUID(), _recordDate: Date, _weather: String? = nil, _dessert: String? = nil, _drink: String? = nil) {
         self.storeName = _storeName
         self.storeTone = _storeTone
+        self.recordID = _recordID
+        self.recordDate = _recordDate
         self.weather = _weather
         self.dessert = _dessert
         self.drink = _drink
