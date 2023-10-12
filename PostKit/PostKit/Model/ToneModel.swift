f08a7d67fb84ea0d9be1e0110c79b523b7c182b7
김다빈 <mokdb17@gmail.com>
2023-10-12 23:50:23 +0900

    [Feat/#7] add Onboarding View

---
+++ PostKit/PostKit/Model/ToneModel.swift
@@ -0,0 +1,8 @@
+//
+//  ToneModel.swift
+//  PostKit
+//
+//  Created by 김다빈 on 10/12/23.
+//
+
+import Foundation
---
+++ PostKit/PostKit/View/OnboardingPage/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  PostKit
+//
+//  Created by 김다빈 on 10/12/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
