eb8c14b53994136c8750ccac647d62f39c547393
whaeun25 <61380136+whaeundo25@users.noreply.github.com>
2023-10-13 00:59:23 +0900

    Revert "[Feat/#7] add Onboarding View"

--- PostKit/PostKit/Persistence.swift
+++ PostKit/PostKit/Persistence.swift
@@ -13,11 +13,9 @@ struct PersistenceController {
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-
-//        for _ in 0..<10 {
-//            let newItem = Item(context: viewContext)
-//            newItem.timestamp = Date()
-//        }
+        for _ in 0..<10 {
+         
+        }
         do {
             try viewContext.save()
         } catch {
