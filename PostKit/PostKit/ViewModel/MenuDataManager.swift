b34649b1bebc8f7092378cde8c82c67fbf7a7f90
HIBAR97 <hibar9710@gmail.com>
2023-10-12 14:25:14 +0900

    [Data/#1] Data Manager 생성

---
+++ PostKit/PostKit/ViewModel/MenuDataManager.swift
@@ -0,0 +1,38 @@
+//
+//  MenuDataManager.swift
+//  PostKit
+//
+//  Created by Kim Andrew on 10/12/23.
+//
+
+import Foundation
+import CoreData
+
+ struct MenuDataManager  {
+
+     static let instance = MenuDataManager()
+        let container: NSPersistentContainer
+        let context: NSManagedObjectContext
+
+        init() {
+            // Container 지정 = Database
+            container = NSPersistentContainer(name: "MenuData")
+            container.loadPersistentStores { description, error in
+                if let error = error {
+                    print("Error loading Core Data. \(error)")
+                }
+            }
+            //Database 저장 관리자
+            context = container.viewContext
+        }
+
+        func save() {
+            do {
+                try context.save()
+                print("Saved Successfully!")
+            } catch let error {
+                print("Saving error. \(error.localizedDescription)")
+            }
+        }
+
+ }
