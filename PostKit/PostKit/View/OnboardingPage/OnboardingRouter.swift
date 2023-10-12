2050fb368bb14e538e80c92c20ece3268b9be79a
김다빈 <mokdb17@gmail.com>
2023-10-12 23:50:23 +0900

    [Feat/#7] add Onboarding View

---
+++ PostKit/PostKit/View/OnboardingPage/OnboardingRouter.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingRouter.swift
+//  PostKit
+//
+//  Created by 김다빈 on 10/12/23.
+//
+
+import SwiftUI
+
+struct OnboardingRouter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingRouter()
+}
