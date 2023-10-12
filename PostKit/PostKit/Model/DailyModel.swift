2eb5d08a737d908ce2381ad4512caedeb014418d
HIBAR97 <hibar9710@gmail.com>
2023-10-12 12:26:43 +0900

    [Data/#1] Data Model 추가

---
+++ PostKit/PostKit/Model/DailyModel.swift
@@ -0,0 +1,25 @@
+//
+//  Daily.swift
+//  PostKit
+//
+//  Created by Kim Andrew on 10/12/23.
+//
+
+import Foundation
+
+final class DailyModel : ObservableObject{
+    let storeName : String
+    let storeTone : String
+
+    @Published var weather : String?
+    @Published var dessert : String?
+    @Published var drink : String?
+
+    init(_storeName: String, _storeTone: String, _weather: String? = nil, _dessert: String? = nil, _drink: String? = nil) {
+        self.storeName = _storeName
+        self.storeTone = _storeTone
+        self.weather = _weather
+        self.dessert = _dessert
+        self.drink = _drink
+    }
+}
---
+++ PostKit/PostKit/Model/MenuModel.swift
@@ -0,0 +1,23 @@
+//
+//  Menu.swift
+//  PostKit
+//
+//  Created by Kim Andrew on 10/12/23.
+//
+
+import Foundation
+
+ final class MenuModel : ObservableObject{
+     let storeName : String
+     let storeTone : String
+
+     @Published var menuName : String
+     @Published var menuPoint : String
+
+     init(_storeName: String, _storeTone: String, _menuName: String, _menuPoint: String) {
+         self.storeName = _storeName
+         self.storeTone = _storeTone
+         self.menuName = _menuName
+         self.menuPoint = _menuPoint
+     }
+ }
