7f40667770dfd5c610dc89f00edcffc197a38e34
whaeundo <1916388@sookmyung.ac.kr>
2023-10-14 17:31:50 +0900

    [Feat/#45] Connect Loading View

--- PostKit/PostKit/View/ResultView.swift
+++ PostKit/PostKit/View/ResultView.swift
@@ -19,67 +19,75 @@ struct ResultView: View {
     private let chatGptService = ChatGptService()
     
     var body: some View {
-        VStack {
-            VStack(alignment: .leading, spacing: 24) {
-                // MARK: - 타이틀 + 설명
-                VStack(alignment: .leading, spacing: 12) {
-                    Text("주문하신 카피가 나왔어요!")
-                        .font(.title1())
-                        .foregroundStyle(Color.gray6)
-                    Text("생성된 피드가 마음에 들지 않는다면\n다시 생성하기 버튼을 통해 새로운 피드를 생성해 보세요.")
-                        .font(.body2Bold())
-                        .foregroundStyle(Color.gray4)
-                }
-                
-                // MARK: - 생성된 카피 출력 + 복사하기 버튼
-                VStack(alignment: .trailing, spacing: 20) {
-                    VStack(alignment: .leading) {
-                        Text(viewModel.promptAnswer)
-                            .multilineTextAlignment(.leading)
-                            .font(.body1Bold())
-                            .foregroundStyle(Color.gray5)
-                        Spacer()
-                    }
-                    .padding(.horizontal, 16)
-                    .padding(.vertical, 20)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .frame(height: 400)
-                    .background(Color.gray1)
-                    .cornerRadius(radius2)
-                    
-                    Button {
-                        copyToClipboard()
-                        // TODO: 버튼 계속 클릭 시 토스트 사라지지 않는 것 FIX 해야함
-                    } label: {
-                        HStack(spacing: 4.0) {
-                            Image(systemName: "doc.on.doc")
-                            Text("복사하기")
+        ZStack{
+            if viewModel.promptAnswer == "생성된 텍스트가 들어가요." {
+                LoadingView()
+            }
+            else{
+                VStack {
+                    VStack(alignment: .leading, spacing: 24) {
+                        // MARK: - 타이틀 + 설명
+                        VStack(alignment: .leading, spacing: 12) {
+                            Text("주문하신 카피가 나왔어요!")
+                                .font(.title1())
+                                .foregroundStyle(Color.gray6)
+                            Text("생성된 피드가 마음에 들지 않는다면\n다시 생성하기 버튼을 통해 새로운 피드를 생성해 보세요.")
+                                .font(.body2Bold())
+                                .foregroundStyle(Color.gray4)
                         }
-                        .foregroundStyle(Color.main)
-                        .font(.body1Bold())
-                        .disabled(isShowingToast)
+                        
+                        // MARK: - 생성된 카피 출력 + 복사하기 버튼
+                        VStack(alignment: .trailing, spacing: 20) {
+                            VStack(alignment: .leading) {
+                                Text(viewModel.promptAnswer)
+                                    .multilineTextAlignment(.leading)
+                                    .font(.body1Bold())
+                                    .foregroundStyle(Color.gray5)
+                                Spacer()
+                            }
+                            .padding(.horizontal, 16)
+                            .padding(.vertical, 20)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .frame(height: 400)
+                            .background(Color.gray1)
+                            .cornerRadius(radius2)
+                            
+                            Button {
+                                copyToClipboard()
+                                // TODO: 버튼 계속 클릭 시 토스트 사라지지 않는 것 FIX 해야함
+                            } label: {
+                                HStack(spacing: 4.0) {
+                                    Image(systemName: "doc.on.doc")
+                                    Text("복사하기")
+                                }
+                                .foregroundStyle(Color.main)
+                                .font(.body1Bold())
+                                .disabled(isShowingToast)
+                            }
+                        }
                     }
+                    Spacer()
+                    
+                    // MARK: - 완료 / 재생성 버튼
+                    CustomDoubleeBtn(leftBtnDescription: "완료", rightBtnDescription: "재생성") {
+                        pathManager.path.removeAll()
+                    } rightAction: {
+                        // TODO: 카피 재생성 기능
+                        regenerateAnswer()
+                    }
+                    .padding(.vertical, 12)
+                    
                 }
+                .padding(.horizontal, paddingHorizontal)
+                .toast(isShowing: $isShowingToast)
             }
-            Spacer()
-            
-            // MARK: - 완료 / 재생성 버튼
-            CustomDoubleeBtn(leftBtnDescription: "완료", rightBtnDescription: "재생성") {
-                pathManager.path.removeAll()
-            } rightAction: {
-                // TODO: 카피 재생성 기능
-                regenerateAnswer()
-            }
-            .padding(.vertical, 12)
-            
         }
-        .padding(.horizontal, paddingHorizontal)
-        .toast(isShowing: $isShowingToast)
     }
     
     // MARK: - Chat GPT API에 재생성 요청
     func regenerateAnswer() {
         Task{
+            viewModel.promptAnswer = "생성된 텍스트가 들어가요."
             self.messages.append(Message(id: UUID(), role: .system, content: "너는 \(appstorageManager.cafeName == "" ? "카페": appstorageManager.cafeName)를 운영하고 있으며 \(appstorageManager.cafeTone == "기본" ? "평범한" : appstorageManager.cafeTone) 말투를 가지고 있어. 글은 존댓말로 작성해줘. 글은 600자 정도로 작성해줘."))
             let newMessage = Message(id: UUID(), role: .user, content: viewModel.prompt)
             self.messages.append(newMessage)
