27cae8ad70c8fd5b3e2e801f1174b2f226a477fb
김다빈 <mokdb17@gmail.com>
2023-10-16 10:14:35 +0900

    [Fix/#54] 패딩값 수정 데이터 점검 화면 연결 점검

--- PostKit/PostKit/View/Setting/SettingToneView.swift
+++ PostKit/PostKit/View/Setting/SettingToneView.swift
@@ -12,9 +12,8 @@ struct SettingToneView: View {
     @EnvironmentObject var pathManager: PathManager
     @EnvironmentObject var appstorageManager: AppstorageManager
     @State private var tone = ""
-    @State private var isActive: Bool = false // 변경사항 생길 시 true
     
-    var tones: [String] = ["기본","친구같은","전문적인","친절한","재치있는","열정적인","감성적인","활발한","세련된","우왕","개쩐다"]
+    var tones: [String] = ["기본","친구같은","전문적인","친절한","재치있는","열정적인","감성적인","활발한","세련된"]
     
     var body: some View {
         VStack(alignment:.leading) {
@@ -24,8 +23,8 @@ struct SettingToneView: View {
             VStack {
                 toggleBtns
                 Spacer()
-                CustomBtn(btnDescription: "저장", isActive: self.$isActive) {
-                    // TODO: 변경 사항 저장
+                CustomBtn(btnDescription: "저장", isActive: .constant(true)) {
+                    pathManager.path.removeLast()
                 }
             }
             .padding(.horizontal,paddingHorizontal)
@@ -36,7 +35,7 @@ struct SettingToneView: View {
 
 extension SettingToneView {
     private var toggleBtns: some View {
-        SelectTone(tone: self.$tone)
+        SelectTone(tone: appstorageManager.$cafeTone)
     }
 }
 
