424df87cf0b99c72d58bf493ba158cab41867234
seoyeon-1206 <ssy_1206@naver.com>
2023-10-16 10:56:00 +0900

    [feat/#56] 디테일 UI 수정

--- PostKit/PostKit/View/DailyView.swift
+++ PostKit/PostKit/View/DailyView.swift
@@ -25,99 +25,105 @@ struct DailyView: View {
     var body: some View {
         VStack {
             CustomHeader(action: {pathManager.path.removeLast()}, title: "일상 카피 생성")
-                .padding(.bottom, paddingHorizontal)
-            
-            VStack(alignment:.leading) {
-                Text("선택한 키워드를 기반으로 카피가 생성됩니다. \n키워드를 선택하지 않을 시 랜덤으로 생성됩니다.")
-                    .font(.body2Bold())
-                    .foregroundStyle(Color.gray4)
-                    .padding(.bottom, 28)
-                
+                .padding(.bottom,20)
+            VStack {
                 ScrollView {
-                    VStack(alignment: .leading) {
-                Text("날씨 / 계절")
-                    .foregroundStyle(Color.gray5)
-                    .font(.body1Bold())
-                    .padding(.bottom, 12)
+                    VStack(alignment:.leading) {
+                        Text("선택한 키워드를 기반으로 카피가 생성됩니다. \n키워드를 선택하지 않을 시 랜덤으로 생성됩니다.")
+                            .font(.body2Bold())
+                            .foregroundStyle(Color.gray4)
+                            .padding(.bottom, 28)
+                        
+                        //                ScrollView {
+                        VStack(alignment: .leading) {
+                            Text("날씨 / 계절")
+                                .foregroundStyle(Color.gray5)
+                                .font(.body1Bold())
+                                .padding(.bottom, 12)
                             Keywords(keyName: KeywordsModel().weatherKeys , selectedIndices: self.$weatherSelected)
-                        .frame(height: isContentsOpened[0] ? 180 : 80)
-                        
-                        if !isContentsOpened[0] {
-                            Button(action: {
-                                withAnimation(.spring()) {
-                                    isContentsOpened[0].toggle()
-                                }
-                            }, label: {
-                                Text("더보기")
-                                    .font(.body2Regular())
-                                    .foregroundStyle(.gray4)
-                                    .underline()
-                            })
+                                .frame(height: isContentsOpened[0] ? 180 : 80)
+                                .padding(.bottom, 12)
+                            
+                            if !isContentsOpened[0] {
+                                Button(action: {
+                                    withAnimation(.spring()) {
+                                        isContentsOpened[0].toggle()
+                                    }
+                                }, label: {
+                                    Text("더보기")
+                                        .font(.body2Regular())
+                                        .foregroundStyle(.gray4)
+                                        .underline()
+                                        .padding(.bottom,24)
+                                })
+                            }
+                            
+                            Divider()
+                                .padding(.bottom, 24)
+                            
+                            Text("커피 / 음료")
+                                .foregroundStyle(Color.gray5)
+                                .font(.body1Bold())
+                                .padding(.bottom, 12)
+                            
+                            Keywords(keyName: KeywordsModel().dailyCoffeeKeys, selectedIndices: self.$dailyCoffeeSelected)
+                                .frame(height: isContentsOpened[1] ? 180 : 80)
+                                .padding(.bottom, 12)
+                            
+                            if !isContentsOpened[1] {
+                                Button(action: {
+                                    withAnimation(.spring()) {
+                                        isContentsOpened[1].toggle()
+                                    }
+                                }, label: {
+                                    Text("더보기")
+                                        .font(.body2Regular())
+                                        .foregroundStyle(.gray4)
+                                        .underline()
+                                        .padding(.bottom,24)
+                                })
+                            }
+                            
+                            Divider()
+                                .padding(.bottom, 24)
+                            Text("디저트")
+                                .foregroundStyle(Color.gray5)
+                                .font(.body1Bold())
+                                .padding(.bottom, 12)
+                            
+                            Keywords(keyName: KeywordsModel().dailyDessertKeys, selectedIndices: self.$dailyDessertSelected)
+                                .frame(height: isContentsOpened[2] ? 140 : 80)
+                                .padding(.bottom, 12)
+                            
+                            
+                            if !isContentsOpened[2] {
+                                Button(action: {
+                                    withAnimation(.spring()) {
+                                        isContentsOpened[2].toggle()
+                                    }
+                                }, label: {
+                                    Text("더보기")
+                                        .font(.body2Regular())
+                                        .foregroundStyle(.gray4)
+                                        .underline()
+                                        .padding(.bottom, paddingBottom)
+                                    
+                                })
+                               
+                            }
                         }
-                
-                    
-                    Divider()
-                        .padding(.bottom, 18)
-                    
-                    Text("커피 / 음료")
-                        .foregroundStyle(Color.gray5)
-                        .font(.body1Bold())
-                        .padding(.bottom, 12)
-                    
-                    Keywords(keyName: KeywordsModel().dailyCoffeeKeys, selectedIndices: self.$dailyCoffeeSelected)
-                            .frame(height: isContentsOpened[1] ? 180 : 80)
-                        
-                        if !isContentsOpened[1] {
-                            Button(action: {
-                                withAnimation(.spring()) {
-                                    isContentsOpened[1].toggle()
-                                }
-                            }, label: {
-                                Text("더보기")
-                                    .font(.body2Regular())
-                                    .foregroundStyle(.gray4)
-                                    .underline()
-                            })
-                        }
-                
-                    
-                        Divider()
-                            .padding(.bottom, 18)
-                    Text("디저트")
-                        .foregroundStyle(Color.gray5)
-                        .font(.body1Bold())
-                        .padding(.bottom, 12)
-                    
-                    Keywords(keyName: KeywordsModel().dailyDessertKeys, selectedIndices: self.$dailyDessertSelected)
-                            .frame(height: isContentsOpened[2] ? 140 : 80)
-                        
-                        if !isContentsOpened[2] {
-                            Button(action: {
-                                withAnimation(.spring()) {
-                                    isContentsOpened[2].toggle()
-                                }
-                            }, label: {
-                                Text("더보기")
-                                    .font(.body2Regular())
-                                    .foregroundStyle(.gray4)
-                                    .underline()
-                            })
-                        }
-                
-                        
-                        Divider()
-                            .padding(.bottom, 18)
+
+                    }
                 }
-            }
-                CustomBtn(btnDescription: "카피생성", isActive: .constant(true), action: {
+                .scrollIndicators(.hidden)
+                CustomBtn(btnDescription: "카피 생성", isActive: .constant(true), action: {
                     sendMessage()
                     pathManager.path.append(.Result)
                 })
                 .padding(.bottom, 12)
-                
-                
             }
             .padding(.horizontal,paddingHorizontal)
+       
         }
         .navigationBarBackButtonHidden()
     }
