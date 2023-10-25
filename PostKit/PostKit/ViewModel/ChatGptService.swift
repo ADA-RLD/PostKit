4511314614873cb36e4649a435b8a479bd6ada9a
김다빈 <mokdb17@gmail.com>
2023-10-25 21:54:32 +0900

    [Fix/#82] Chatgpt 성능향상

--- PostKit/PostKit/ViewModel/ChatGptService.swift
+++ PostKit/PostKit/ViewModel/ChatGptService.swift
@@ -11,12 +11,18 @@ import Alamofire
 class ChatGptService {
     private let baseUrl = "https://api.openai.com/v1/chat/completions"
     
+    func getRandomKey() -> String {
+        let randomIndex = Int.random(in: 0..<Constants.ChatGptAPIKey.count)
+        return Constants.ChatGptAPIKey[randomIndex]
+    }
+    
     func sendMessage(messages: [Message]) async -> chatGptResponse? {
         let openAIMessages = messages.map({chatGptMessage(role: .system, content: $0.content)})
-        
-        let body = chatGptBody(model: "gpt-4", messages: openAIMessages)
+        let randomKey = getRandomKey()
+        print(randomKey)
+        let body = chatGptBody(model: "gpt-3.5-turbo-0301", messages: openAIMessages)
         let headers: HTTPHeaders =  [
-            "Authorization" : "Bearer \(Constants.ChatGptAPIKey)"
+            "Authorization" : "Bearer \(randomKey)"
         ]
         
         return try? await AF.request(baseUrl, method: .post, parameters: body, encoder: .json, headers: headers).serializingDecodable(chatGptResponse.self).value
