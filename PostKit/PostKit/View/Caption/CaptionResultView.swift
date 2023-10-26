42747a464bced9d30f44ee56de2d1447cd0c3a7d
HIBAR97 <hibar9710@gmail.com>
2023-10-26 21:24:19 +0900

    [Data/#77] CaptionResult Save문 완성

--- PostKit/PostKit/View/Caption/CaptionResultView.swift
+++ PostKit/PostKit/View/Caption/CaptionResultView.swift
@@ -18,6 +18,9 @@ struct CaptionResultView: View {
     @ObservedObject var viewModel = ChatGptViewModel.shared
     private let chatGptService = ChatGptService()
     
+    //CoreData Manager
+    let coreDataManager = CoreDataManager.instance
+    
     var body: some View {
         ZStack{
             if viewModel.promptAnswer == "생성된 텍스트가 들어가요." {
@@ -156,6 +159,24 @@ extension View {
     func toast(isShowing: Binding<Bool>, duration: TimeInterval = 1.5) -> some View {
         modifier(ToastModifier(isShowing: isShowing, duration: duration))
     }
+}
+
+extension CaptionResultView : CaptionResultProtocol {
+    
+    func saveCaptionResult(Id: UUID, category: String, Result: String) {
+        let newCaption = CaptionResult(context: coreDataManager.context)
+        newCaption.resultId = Id
+        newCaption.date = Date()
+        newCaption.category = category
+        newCaption.caption = Result
+        print("Caption 저장 완료!\n resultId : \(newCaption.resultId)\n Date : \(newCaption.date)\n Category : \(newCaption.category)\n Caption : \(newCaption.caption)")
+        coreDataManager.save()
+    }
+    
+    func fetchCaptionResult(category: String) {
+        print("Fetch 불필요")
+    }
+    
 }
 
 #Preview {
