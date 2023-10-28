40f31b997893767b85b503e7a462c487f0099881
김다빈 <mokdb17@gmail.com>
2023-10-28 16:06:10 +0900

    [Chore/#94] 코드 컨벤션 맞춤

--- PostKit/PostKit/View/Onboarding/OnboardingTone.swift
+++ PostKit/PostKit/View/Onboarding/OnboardingTone.swift
@@ -15,24 +15,30 @@ struct OnboardingTone: View {
     
     var body: some View {
         VStack(alignment:.leading,spacing: 0) {
+            
             OnboardingCustomHeader(action: onboardingRouter.previousPage)
+            
             ContentArea {
                 VStack(alignment: .leading,spacing: 40) {
+                    
                     VStack(alignment: .leading,spacing: 12) {
+                        
                         Text("원하는 톤을 선택하세요")
                             .font(.title1())
                             .foregroundStyle(Color.gray6)
+                        
                         Text("선택한 톤을 바탕으로 카피가 생성됩니다.")
                             .font(.body2Bold())
                             .foregroundStyle(Color.gray4)
                     }
+                    
                     SelectTone(selectedTones: $cafeTone)
                 }
             }
             
             Spacer()
 
-            CTABtn(btnLabel: "다음", isActive: .constant(true), action: {print(cafeTone);onboardingRouter.nextPage()})
+            CTABtn(btnLabel: "다음", isActive: .constant(true), action: {onboardingRouter.nextPage()})
         }
     }
 }
