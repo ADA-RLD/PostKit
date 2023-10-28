599f9edf8061cff647e2c9cf782b579742de0ccb
seoyeon-1206 <ssy_1206@naver.com>
2023-10-28 20:08:18 +0900

    [feat/#97]CustomTextfield 변수명 수정, HashtagView UI 구현

---
+++ PostKit/PostKit/View/Hashtag/HashtagView.swift
@@ -0,0 +1,18 @@
+//
+//  HashtagView.swift
+//  PostKit
+//
+//  Created by 신서연 on 2023/10/28.
+//
+
+import SwiftUI
+
+struct HashtagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HashtagView()
+}
