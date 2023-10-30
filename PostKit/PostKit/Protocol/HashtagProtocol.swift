93a30b6a26fe815fef97ff8ed24786fa7b07eb24
HIBAR97 <hibar9710@gmail.com>
2023-10-30 22:05:38 +0900

    [Data/#113] HashtagProtocol 추가

---
+++ PostKit/PostKit/Protocol/HashtagProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  HashtagProtocol.swift
+//  PostKit
+//
+//  Created by Kim Andrew on 10/30/23.
+//
+
+import Foundation
+
+protocol HashtagProtocol {
+    
+    func FetchHashtag()
+    
+    func SaveHashtag()
+    
+}
