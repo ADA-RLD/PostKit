163c6f5845c2651486c39ff2ae62998c8e5091d1
whaeun25 <61380136+whaeundo25@users.noreply.github.com>
2023-10-31 11:22:15 +0900

    Update DailyView.swift

--- PostKit/PostKit/View/Caption/DailyView.swift
+++ PostKit/PostKit/View/Caption/DailyView.swift
@@ -153,7 +153,7 @@ extension DailyView {
         print("생성 정보 :\nStore Name : \(storeModel.storeName)\nTone : \(storeModel.tone)")
         // TODO: COREDATA 변경필요
         if storeModel.tone.contains("기본") {
-            self.messages.append(Message(id: UUID(), role: .system, content: "너는 \(storeModel.storeName == "" ? "카페": storeModel.storeName)를 운영하고 있으며 평범한 말투를 가지고 있어. 글은 존댓말로 작성해줘. 꼭 글자수는 150000000자 정도로 작성해줘."))
+            self.messages.append(Message(id: UUID(), role: .system, content: "너는 \(storeModel.storeName == "" ? "카페": storeModel.storeName)를 운영하고 있으며 평범한 말투를 가지고 있어. 글은 존댓말로 작성해줘. 꼭 글자수는 150자 정도로 작성해줘."))
         }
         else{
             self.messages.append(Message(id: UUID(), role: .system, content: "너는 \(storeModel.storeName == "" ? "카페": storeModel.storeName)"))
