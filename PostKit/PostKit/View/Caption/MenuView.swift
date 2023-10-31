756adaa40bb187eafc20a662baec3f53a3a940d9
whaeundo <1916388@sookmyung.ac.kr>
2023-10-31 11:05:17 +0900

    [Refactor] Disunite function

--- PostKit/PostKit/View/Caption/MenuView.swift
+++ PostKit/PostKit/View/Caption/MenuView.swift
@@ -108,62 +108,74 @@ struct MenuView: View {
         }
         .navigationBarBackButtonHidden()
     }
-    
+}
+
+
+//MARK: extension Function
+extension MenuView {
     // MARK: - Chat Gpt API에 응답 요청
     func sendMessage(){
         Task{
-            var pointText = ""
-            
-            print("생성 정보 :\nStore Name : \(storeModel.storeName)\nTone : \(storeModel.tone)")
-//            TODO: coreData변경 필요
-            if storeModel.tone.contains("기본") {
-                self.messages.append(Message(id: UUID(), role: .system, content: "너는 \(storeModel.storeName == "" ? "카페": storeModel.storeName)를 운영하고 있으며 평범한 말투를 가지고 있어. 글은 존댓말로 작성해줘. 꼭 글자수는 150자 정도로 작성해줘."))
-            }else{
-                
-                self.messages.append(Message(id: UUID(), role: .system, content: "너는 \(storeModel.storeName == "" ? "카페": storeModel.storeName)"))
-
-                  for _tone in storeModel.tone {
-                      self.messages.append(Message(id: UUID(), role: .system, content: "\(_tone == "기본" ? "평범한": _tone)"))
-                  }
-                
-                self.messages.append(Message(id: UUID(), role: .system, content:"말투를 가지고 있어. 글은 존댓말로 작성해줘. 꼭 글자수는 150자 정도로 작성해줘."))
-            }
-            
-            if !coffeeSelected.isEmpty {
-                pointText = pointText + "이 메뉴의 특징으로는 "
-                for index in coffeeSelected.indices {
-                    pointText = pointText + "\(coffeeSelected[index]), "
-                }
-                pointText = pointText + "이 있어."
-            }
-            else if !drinkSelected.isEmpty {
-                pointText = pointText + "이 메뉴의 특징으로는 "
-                for index in drinkSelected.indices {
-                    pointText = pointText + "\(drinkSelected[index]), "
-                }
-                pointText = pointText + "이 있어."
-            }
-            else if !dessertSelected.isEmpty {
-                pointText = pointText + "이 메뉴의 특징으로는 "
-                for index in dessertSelected.indices {
-                    pointText = pointText + "\(dessertSelected[index]), "
-                }
-                pointText = pointText + "이 있어."
-            }
-            
-            self.currentInput = "메뉴의 이름은 \(menuName)인 메뉴에 대해서 인스타그램 피드를 작성해줘. \(pointText)"
-            let newMessage = Message(id: UUID(), role: .user, content: self.currentInput)
-            self.messages.append(newMessage)
-            viewModel.prompt = self.currentInput
-            self.currentInput = ""
-            let response = await chatGptService.sendMessage(messages: self.messages)
-            viewModel.promptAnswer = response?.choices.first?.message.content == nil ? "" : response!.choices.first!.message.content
-            viewModel.category = "Menu"
-            print(response?.choices.first?.message.content as Any)
-            print(response as Any)
+            createPrompt()
+            self.messages.append(Message(id: UUID(), role: .user, content: self.currentInput))
+            await createCaption()
         }
     }
+    
+    func createPrompt(){
+        var pointText = ""
+        
+        print("생성 정보 :\nStore Name : \(storeModel.storeName)\nTone : \(storeModel.tone)")
+//            TODO: coreData변경 필요
+        if storeModel.tone.contains("기본") {
+            self.messages.append(Message(id: UUID(), role: .system, content: "너는 \(storeModel.storeName == "" ? "카페": storeModel.storeName)를 운영하고 있으며 평범한 말투를 가지고 있어. 글은 존댓말로 작성해줘. 꼭 글자수는 150자 정도로 작성해줘."))
+        }else{
+            
+            self.messages.append(Message(id: UUID(), role: .system, content: "너는 \(storeModel.storeName == "" ? "카페": storeModel.storeName)"))
+
+              for _tone in storeModel.tone {
+                  self.messages.append(Message(id: UUID(), role: .system, content: "\(_tone == "기본" ? "평범한": _tone)"))
+              }
+            
+            self.messages.append(Message(id: UUID(), role: .system, content:"말투를 가지고 있어. 글은 존댓말로 작성해줘. 꼭 글자수는 150자 정도로 작성해줘."))
+        }
+        
+        if !coffeeSelected.isEmpty {
+            pointText = pointText + "이 메뉴의 특징으로는 "
+            for index in coffeeSelected.indices {
+                pointText = pointText + "\(coffeeSelected[index]), "
+            }
+            pointText = pointText + "이 있어."
+        }
+        else if !drinkSelected.isEmpty {
+            pointText = pointText + "이 메뉴의 특징으로는 "
+            for index in drinkSelected.indices {
+                pointText = pointText + "\(drinkSelected[index]), "
+            }
+            pointText = pointText + "이 있어."
+        }
+        else if !dessertSelected.isEmpty {
+            pointText = pointText + "이 메뉴의 특징으로는 "
+            for index in dessertSelected.indices {
+                pointText = pointText + "\(dessertSelected[index]), "
+            }
+            pointText = pointText + "이 있어."
+        }
+        
+        self.currentInput = "메뉴의 이름은 \(menuName)인 메뉴에 대해서 인스타그램 피드를 작성해줘. \(pointText)"
+    }
+    
+    func createCaption() async{
+        viewModel.prompt = self.currentInput
+        self.currentInput = ""
+        let response = await chatGptService.sendMessage(messages: self.messages)
+        viewModel.promptAnswer = response?.choices.first?.message.content == nil ? "" : response!.choices.first!.message.content
+        viewModel.category = "Menu"
+        print(response?.choices.first?.message.content as Any)
+        print(response as Any)
+    }
 }
+
 extension View {
     func hideKeyboard() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
