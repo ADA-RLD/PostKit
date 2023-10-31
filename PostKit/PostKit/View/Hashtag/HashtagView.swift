82167d2dd8b40c94eca35886e7d350453c4392a9
seoyeon-1206 <ssy_1206@naver.com>
2023-10-31 20:03:46 +0900

    [feat/#121] HashtagView에 HashtagPopover 추가

--- PostKit/PostKit/View/Hashtag/HashtagView.swift
+++ PostKit/PostKit/View/Hashtag/HashtagView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct HashtagView: View {
     @EnvironmentObject var pathManager: PathManager
@@ -15,73 +14,153 @@ struct HashtagView: View {
     @State private var isActive: Bool = false
     @State private var locationTags: [String] = []
     @State private var emphasizeTags: [String] = []
+    @State private var showingAlert = false
     
-    //CoreData Manager
-    private let coreDataManager = CoreDataManager.instance
-    
-    //CoreData Class
-    @State private var hashtags: [HashtagModel] = []
+    @State private var isShowingDescription = false
+    @State private var popupState: PopOverType = .region
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            CustomHeader(action: {pathManager.path.removeLast()}, title: "해시태그 생성")
-            
-            ScrollView {
-                ContentArea {
-                    VStack(alignment: .leading, spacing: 28) {
-                        Text("입력한 지역명을 기반으로 해시태그가 생성됩니다. \n강조 키워드 미입력 시 기본 키워드만의 조합으로 생성됩니다.")
-                            .font(.body2Bold())
-                            .foregroundColor(.gray4)
-                        
+        ZStack {
+        //TODO: GeometryReader 활용 해상도 수정 필요
+            VStack(alignment: .leading, spacing: 0) {
+                CustomHeader(action: {pathManager.path.removeLast()}, title: "해시태그 생성")
+                
+                ScrollView {
+                    ContentArea {
                         VStack(alignment: .leading, spacing: 28) {
-                            VStack(alignment: .leading, spacing: 12) {
-                                Text("지역명 *")
-                                    .font(.body1Bold())
-                                    .foregroundColor(.gray5)
-                                CustomTextfield(text: $locationText, placeHolder: "한남동", customTextfieldState: .reuse) {
-                                    if !locationText.isEmpty {
-                                        locationTags.append(locationText)
-                                        checkTags()
-                                    }
-                                }
-                            }
-                            WrappingHStack(alignment: .leading) {
-                                ForEach(locationTags, id: \.self) { tag in
-                                    CustomHashtag(tagText: tag) {
-                                        locationTags.removeAll(where: { $0 == tag })
-                                        checkTags()
-                                    }
-                                }
-                            }
+                            Text("입력한 지역명을 기반으로 해시태그가 생성됩니다. \n강조 키워드 미입력 시 기본 키워드만의 조합으로 생성됩니다.")
+                                .font(.body2Bold())
+                                .foregroundColor(.gray4)
                             
-                            VStack(alignment: .leading, spacing: 12) {
-                                Text("강조키워드")
-                                    .font(.body1Bold())
-                                    .foregroundColor(.gray5)
-                                CustomTextfield(text: $emphasizeText, placeHolder: "마카롱", customTextfieldState: .reuse) {
-                                    if !emphasizeText.isEmpty {
-                                        emphasizeTags.append(emphasizeText)
+                            VStack(alignment: .leading, spacing: 28) {
+                                VStack(alignment: .leading, spacing: 12) {
+                                    HStack {
+                                        Text("지역명 *")
+                                            .font(.body1Bold())
+                                            .foregroundColor(.gray5)
+                                        Spacer()
+                                        HashtagPopover(isShowingDescription: $isShowingDescription)
+                                            .border(.red)
                                         
                                     }
+                                    ZStack(alignment: .topLeading) {
+                                        CustomTextfield(text: $locationText, placeHolder: "한남동", customTextfieldState: .reuse) {
+                                            if !locationText.isEmpty && locationTags.count <= 4 {
+                                                locationTags.append(locationText)
+                                                checkTags()
+                                            } else if locationTags.count > 4 {
+                                                showingAlert = true
+                                            }
+                                        }
+                                        .alert(isPresented: $showingAlert) {
+                                            Alert(title: Text(""), message: Text("최대 5개까지만 입력할 수 있습니다."),
+                                                  dismissButton: .default(Text("확인")))
+                                        }
+                                    }
+                                    Text("최대 5개까지 작성가능합니다.")
+                                        .font(.body2Regular())
+                                        .foregroundColor(.gray3)
+                                    
+                                    WrappingHStack(alignment: .leading) {
+                                        ForEach(locationTags, id: \.self) { tag in
+                                            CustomHashtag(tagText: tag) {
+                                                locationTags.removeAll(where: { $0 == tag })
+                                                checkTags()
+                                            }
+                                        }
+                                    }
                                 }
-                            }
-                            WrappingHStack(alignment: .leading) {
-                                ForEach(emphasizeTags, id: \.self) { tag in
-                                    CustomHashtag(tagText: tag) {
-                                        emphasizeTags.removeAll(where: { $0 == tag })
+                                
+                                
+                                VStack(alignment: .leading, spacing: 12) {
+                                    Text("강조키워드")
+                                        .font(.body1Bold())
+                                        .foregroundColor(.gray5)
+                                    ZStack(alignment: .topLeading) {
+                                        CustomTextfield(text: $emphasizeText, placeHolder: "마카롱", customTextfieldState: .reuse) {
+                                            if !emphasizeText.isEmpty && emphasizeTags.count <= 4 {
+                                                emphasizeTags.append(emphasizeText)
+                                            } else if emphasizeTags.count > 4 {
+                                                showingAlert = true
+                                            }
+                                        }
+                                        .alert(isPresented: $showingAlert) {
+                                            Alert(title: Text(""), message: Text("최대 5개까지만 입력할 수 있습니다."),
+                                                  dismissButton: .default(Text("확인")))
+                                        }
+                                    }
+                                    Text("최대 5개까지 작성가능합니다.")
+                                        .font(.body2Regular())
+                                        .foregroundColor(.gray3)
+                                    
+                                    WrappingHStack(alignment: .leading) {
+                                        ForEach(emphasizeTags, id: \.self) { tag in
+                                            CustomHashtag(tagText: tag) {
+                                                emphasizeTags.removeAll(where: { $0 == tag })
+                                            }
+                                        }
                                     }
                                 }
                             }
                         }
+                        .scrollIndicators(.hidden)
                     }
                 }
-                .scrollIndicators(.hidden)
+                Spacer()
+                CTABtn(btnLabel: "해시태그 생성", isActive: self.$isActive, action: {})
+                
+                    .navigationBarBackButtonHidden()
             }
-            Spacer()
-            CTABtn(btnLabel: "해시태그 생성", isActive: self.$isActive, action: {pathManager.path.append(.HashtagResult)})
+            
+            popoverView(.region)
+                .frame(maxWidth: .infinity)
+                .frame(maxHeight: .infinity)
+                .background(Color.gray5.opacity(0.3))
+            
         }
-        .onAppear{FetchHashtag()}
-        .navigationBarBackButtonHidden()
+       
+    }
+    
+    
+    @ViewBuilder
+    func popoverView(_ type: PopOverType) -> some View {
+        VStack(spacing: 0) {
+            TriangleView()
+                .foregroundColor(.gray1)
+                .frame(width: 18, height: 12)
+            ZStack() {
+                RoundedRectangle(cornerRadius: radius1)
+                    .frame(width: 200, height: 167)
+                    .foregroundColor(.gray1)
+                
+                VStack(alignment: .leading, spacing: 6) {
+                    
+                    switch type {
+                    case .region:
+                        VStack {
+                            ForEach(0..<type.description.count, id: \.self) { index in
+                                if index % 2 == 1 {
+                                    Text(type.description[index])
+                                        .foregroundColor(.gray5)
+                                        .font(.body2Bold())
+                                }
+                                else {
+                                    Text(type.description[index])
+                                        .foregroundColor(.gray4)
+                                        .font(.body2Regular())
+                                }
+                            }
+                        }
+                    case .keyword:
+                        EmptyView()
+                    }
+                }
+                .frame(width: 166, height: 119)
+                
+            }
+            .offset(type.offset)
+            
+        }
     }
     
     private func checkTags() {
@@ -93,41 +172,6 @@ struct HashtagView: View {
     }
 }
 
-extension HashtagView : HashtagProtocol {
-    
-    func convertDayTime(time: Date) -> Date {
-        let today = Date()
-        let timezone = TimeZone.autoupdatingCurrent
-        let secondsFromGMT = timezone.secondsFromGMT(for: today)
-        let localizedDate = today.addingTimeInterval(TimeInterval(secondsFromGMT))
-        return localizedDate
-    }
-    
-    func FetchHashtag() {
-        let HashtagRequest = NSFetchRequest<HashtagData>(entityName: "HashtagData")
-        
-        do {
-            let hashtagDataArray = try coreDataManager.context.fetch(HashtagRequest)
-            hashtags = hashtagDataArray.map{ hashtagCoreData in
-                return HashtagModel(
-                    _id: hashtagCoreData.resultId ?? UUID(),
-                    _date: hashtagCoreData.date ?? Date(),
-                    _locationTag: hashtagCoreData.locationTag ?? [""],
-                    _keyword: hashtagCoreData.keyword ?? [""],
-                    _hashtag: hashtagCoreData.hashtag ?? ""
-                )
-            }
-        } catch {
-            print("ERROR STORE CORE DATA")
-            print(error.localizedDescription)
-        }
-    }
-    
-    func SaveHashtag(date: Date, locationTag: Array<String>, keyword: Array<String>, Result: String) {
-        //결과는 HashtagResultView에서 저장합니다.
-    }
-    
-}
 
 #Preview {
     HashtagView()
