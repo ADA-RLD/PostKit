e4854b8bb7290a70972f54b4ed7b163e718bbdbb
김다빈 <mokdb17@gmail.com>
2023-11-01 22:41:24 +0900

    AppDelegate 파일 추가

---
+++ PostKit/PostKit/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  PostKit
+//
+//  Created by 김다빈 on 11/1/23.
+//
+
+import Foundation
+import UIKit
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    // ...
+    return true
+  }
+}
