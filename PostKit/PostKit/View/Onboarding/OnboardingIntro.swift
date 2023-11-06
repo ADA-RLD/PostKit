6c706194ed69adbd49ee14a882f5854b906f5d75
김다빈 <mokdb17@gmail.com>
2023-11-06 11:24:26 +0900

    [Chore/#175] 온보딩 뷰 텍스트 수정

--- PostKit/PostKit/View/Onboarding/OnboardingIntro.swift
+++ PostKit/PostKit/View/Onboarding/OnboardingIntro.swift
@@ -20,10 +20,12 @@ struct OnboardingIntro: View {
                         .resizable()
                         .scaledToFit()
                         .frame(height: 113)
+                    
                     Text("포스트킷과 함께\n우리 매장을 알려봐요")
                         .font(.title1())
                         .foregroundStyle(Color.gray6)
-                    Text("포스트킷은 쉽고 빠르게 마케팅용 콘텐츠를 생성하는\n효과적인 인공지능 글쓰기 도구입니다.\n\n몇 번의 탭만으로\n생성하고 싶은 주제 대한 정보를 선택하고\n귀찮은 콘텐츠 생성 업무를 해결해보세요!")
+                    
+                    Text("포스트킷은 쉽고 빠르게 마케팅용 콘텐츠를 생성하는\n효과적인 인공지능 글쓰기 도구입니다.\n\n몇 번의 탭만으로\n귀찮은 콘텐츠 생성 업무를 해결해보세요!")
                         .font(.body1Bold())
                         .foregroundStyle(Color.gray4)
                 }
