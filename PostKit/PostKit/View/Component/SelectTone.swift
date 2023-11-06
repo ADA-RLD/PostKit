5ec6a2ac27bee59dc0860ae6f50e1fbfd5b06157
김다빈 <mokdb17@gmail.com>
2023-11-06 20:45:53 +0900

    [Design/#189] OnboardingView,SettingView 수정

--- PostKit/PostKit/View/Component/SelectTone.swift
+++ PostKit/PostKit/View/Component/SelectTone.swift
@@ -8,21 +8,22 @@
 import SwiftUI
 
 struct SelectTone: View {
+    @Binding var selectedTones: [String]
+    
     //TODO: 임시데이터라 데이터 변경이 필요합니다
     let tones: [Tone] = [
-        Tone(tone: "친절한", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
-        Tone(tone: "친절한", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
-        Tone(tone: "친절한", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
-        Tone(tone: "친절한", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: true),
-        Tone(tone: "친절한", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
-        Tone(tone: "친절한", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
-        Tone(tone: "친절한", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
-        Tone(tone: "친절한", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
-        Tone(tone: "친절한", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
-        Tone(tone: "친절한", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false)
+        Tone(tone: "1", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
+        Tone(tone: "2", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
+        Tone(tone: "3", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
+        Tone(tone: "4", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
+        Tone(tone: "5", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: true),
+        Tone(tone: "6", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
+        Tone(tone: "7", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
+        Tone(tone: "8", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
+        Tone(tone: "9", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false),
+        Tone(tone: "10", toneExample: "우리 카페, 맛과 분위기에 걸맞는 퀄리티! ☕✨", isBest: false)
     ]
     
-    @Binding var selectedTones: [String]
     var body: some View {
         toggleBtns
     }
@@ -31,11 +32,12 @@ struct SelectTone: View {
 //MARK: Extension: View
 extension SelectTone {
     private var toggleBtns: some View {
-        VStack(alignment:.leading, spacing: 12) {
+        VStack(alignment: .leading, spacing: 12) {
             // 배열에 추가되면 자동으로 생성하게 해주는 기능을 만들었어요!
             ForEach(tones, id: \.self) { rowIndex in
                 toggleBtn(concept: rowIndex.tone, conceptExample: rowIndex.toneExample, isBest: rowIndex.isBest) {
                     addTone(tone: rowIndex.tone)
+                    print(selectedTones)
                 }
             }
         }
@@ -50,13 +52,14 @@ extension SelectTone {
                 .stroke(selectedTones.contains(concept) ? Color.main: Color.gray1, lineWidth: 2)
                 .background(
                     RoundedRectangle(cornerRadius: radius2)
-                        .fill(selectedTones.contains(concept) ? Color.sub: Color.gray1)
+                        .fill(selectedTones.contains(concept) ? Color.sub : Color.gray1)
                 )
+                // TODO: padding이 개념인 듯..?
                 .frame(height: 84)
                 .foregroundColor(Color.gray1)
                 .overlay(alignment: .leading) {
-                    VStack(alignment:.leading,spacing: 8) {
-                        HStack(spacing:8) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack(spacing: 8) {
                             Text(concept)
                                 .font(.body1Bold())
                                 .foregroundColor(selectedTones.contains(concept) ? Color.gray5 : Color.gray4)
@@ -70,30 +73,25 @@ extension SelectTone {
                             }
                         }
                         Text(conceptExample)
-                            .foregroundColor(Color.gray4)
-                            .font(.body2Regular())
+                            .body2Regular(textColor: .gray4)
                     }
-                    .padding(.all,20)
+                    .padding(.all, 20)
                 }
         }
     }
 }
 
-// MARK: Extension: Function
+// MARK: Extension: Function => view model로 빼주세요...
 extension SelectTone {
     private func addTone(tone: String) {
-        
         if !selectedTones.contains(tone) {
-            
             if selectedTones.count >= 3 {
                 selectedTones.removeFirst()
                 selectedTones.append(tone)
             }
-            
             else {
                 selectedTones.append(tone)
             }
-            
         }
         else {
             if let firstIndex = selectedTones.firstIndex(of: tone) {
