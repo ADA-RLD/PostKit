d977c8e285ca0e3ee99afca1af84eb70a5a69fce
Padoari <padoari85@gmail.com>
2023-11-07 21:52:42 +0900

    [Design/#203] MainCaptionView Hi-Fi 적용
    완벽한듯 >w<

---
+++ PostKit/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  PostKit
+//
+//  Created by 김다빈 on 10/25/23.
+//
+
+import Foundation
+
+enum Constants {
+    static let ChatGptAPIKey: [String] = [
+        "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",
+]
+    
+    static let MixpanelToken: String = "b072cc67817594f7e8a51bbf5706ca84"
+}
