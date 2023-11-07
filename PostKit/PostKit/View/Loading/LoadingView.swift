a98d87a748eb8ea68b467f2e398553a8d4578106
Padoari <padoari85@gmail.com>
2023-11-07 23:35:00 +0900

    [Fix/#203] LoadingView Mixpanel 추가
    머지할때 사라진거 추가했습니다

--- PostKit/PostKit/View/Loading/LoadingView.swift
+++ PostKit/PostKit/View/Loading/LoadingView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Mixpanel
 
 struct LoadingView: View {
     
@@ -16,16 +17,17 @@ struct LoadingView: View {
     //디버깅용 데이터 삭제하지는 말아주세요.
     //private var SampleData: [String] = ["1번 친구","2번 친구","3번 친구","4번 친구","5번 친구"]
     @ObservedObject var loadingModel = LoadingViewModel.shared
+    @EnvironmentObject var pathManager: PathManager
     
     var body: some View {
         
         let Tips: [TipStruck] = [
             TipStruck(tipNum: 0, tipTitle: "방문자 수를 높이는 프로필 셋팅", tips: "호기심을 자극하는 프로필 사진을 설정하세요\n계정의 주목도를 높여주는 스토리를 활용하세요\n검색에 잘 걸리는 프로필 이름을 사용하세요"),
-             TipStruck(tipNum: 1, tipTitle: "주목도를 높이는 인스타그램 스토리", tips: "스토리를 업로드할 경우 프로필 사진에 띠가 생겨요 24시간동안만 유지되어 부담없이 업로드할 수 있고,스토리가 없을 때보다 프로필이 눈에 띄어요!"),
-             TipStruck(tipNum: 2, tipTitle: "스토리에 올릴 콘텐츠 추천", tips: "현재 상황을 생동감 넘치는 영상으로 찍어 올려보세요\n고객의 후기를 공유해보세요\n내 피드 게시물을 다시 공유해보세요"),
-             TipStruck(tipNum: 3, tipTitle: "유입을 높이는 프로필 이름 만들기", tips: "강조하고 싶은 유입 키워드와 매장 이름의 조합으로 프로필 이름을 만들어보세요!\n계정태그와 해시태그를 적절히 활용하세요"),
-             TipStruck(tipNum: 4, tipTitle: "스토리 하이라이트 기능", tips: "업로드한 스토리를 24시간 후에도 고정시켜 노출할 수 있는 방법이 있어요! 하이라이트를 활용해 스토리를 카테고리화하여 어필하세요")
-         ]
+            TipStruck(tipNum: 1, tipTitle: "주목도를 높이는 인스타그램 스토리", tips: "스토리를 업로드할 경우 프로필 사진에 띠가 생겨요 24시간동안만 유지되어 부담없이 업로드할 수 있고,스토리가 없을 때보다 프로필이 눈에 띄어요!"),
+            TipStruck(tipNum: 2, tipTitle: "스토리에 올릴 콘텐츠 추천", tips: "현재 상황을 생동감 넘치는 영상으로 찍어 올려보세요\n고객의 후기를 공유해보세요\n내 피드 게시물을 다시 공유해보세요"),
+            TipStruck(tipNum: 3, tipTitle: "유입을 높이는 프로필 이름 만들기", tips: "강조하고 싶은 유입 키워드와 매장 이름의 조합으로 프로필 이름을 만들어보세요!\n계정태그와 해시태그를 적절히 활용하세요"),
+            TipStruck(tipNum: 4, tipTitle: "스토리 하이라이트 기능", tips: "업로드한 스토리를 24시간 후에도 고정시켜 노출할 수 있는 방법이 있어요! 하이라이트를 활용해 스토리를 카테고리화하여 어필하세요")
+        ]
         
         VStack {
             VStack(alignment: .leading, spacing: 12){
@@ -79,6 +81,26 @@ struct LoadingView: View {
                     hashTagTimer.invalidate()
                     print("\nCaption 생성시간 : \(count)초\n")
                 }
+            }
+            if pathManager.path.contains(.Daily) {
+                Mixpanel.mainInstance().time(event: "일상 글 로딩")
+            }
+            else if pathManager.path.contains(.Menu) {
+                Mixpanel.mainInstance().time(event: "메뉴 글 로딩")
+            }
+            else if pathManager.path.contains(.Hashtag) {
+                Mixpanel.mainInstance().time(event: "해시태그 글 로딩")
+            }
+        }
+        .onDisappear {
+            if pathManager.path.contains(.Daily) {
+                Mixpanel.mainInstance().track(event: "일상 글 로딩")
+            }
+            else if pathManager.path.contains(.Menu) {
+                Mixpanel.mainInstance().track(event: "메뉴 글 로딩")
+            }
+            else if pathManager.path.contains(.Hashtag) {
+                Mixpanel.mainInstance().track(event: "해시태그 글 로딩")
             }
         }
     }
