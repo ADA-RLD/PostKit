5dd87ccc390ba8d85044332309b0f3666022897e
김다빈 <mokdb17@gmail.com>
2023-11-07 09:43:45 +0900

    [Chore/#189] 코드리뷰 반영

--- PostKit/PostKit/View/Onboarding/OnboardingStore.swift
+++ PostKit/PostKit/View/Onboarding/OnboardingStore.swift
@@ -18,7 +18,7 @@ struct OnboardingStore: View {
         VStack(spacing: 0) {
             OnboardingCustomHeader(action: {onboardingRouter.previousPage()})
             ContentArea {
-                VStack(alignment:.leading, spacing: 40){
+                VStack(alignment:.leading, spacing: 40) {
 
                     VStack(alignment:.leading, spacing: 12) {
                         Text("매장의 이름을 알려주세요")
