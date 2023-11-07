351c15ca23ece836ff407698a38206469cc30e0d
HIBAR97 <hibar9710@gmail.com>
2023-11-07 09:28:40 +0900

    [Design/#185] LoadingCancelView 완성

--- PostKit/PostKit/View/Loading/LoadingCancelView.swift
+++ PostKit/PostKit/View/Loading/LoadingCancelView.swift
@@ -10,30 +10,39 @@ import SwiftUI
 struct LoadingCancelView: View {
     var body: some View {
         ContentArea {
-            VStack(alignment: .center, spacing: 8){
-                Text("생성을 취소할까요?")
-                    .font(.body1Bold())
-                    .foregroundColor(.gray6)
-                
-                Text("취소하더라도 1 크레딧이 사용돼요")
-                
-                ZStack{
-                    Rectangle()
-                        .cornerRadius(radius2)
+            ZStack{
+                RoundedRectangle(cornerRadius: radius2)
+                    .stroke(.black, lineWidth: 1)
+                    .foregroundColor(.gray1)
+                    .frame(width: 280,height: 261)
+                   
+                VStack(alignment: .center, spacing: 8){
+                    Text("생성을 취소할까요?")
+                        .font(.body1Bold())
+                        .foregroundColor(.gray6)
+                    
+                    Text("취소하더라도 1 크레딧이 사용돼요")
+                        .font(.body2Bold())
+                        .foregroundColor(.gray4)
+                    
+                    RoundedRectangle(cornerRadius: radius2)
                         .foregroundColor(.gray5)
                         .padding(.horizontal, 100)
                         .padding(.vertical, 20)
+                        .overlay(alignment: .center) {
+                            Text("취소")
+                                .body2Bold(textColor: .white)
+                        }
+                        .frame(height: 90)
                     
-                    Text("취소")
+                    Text("계속 생성")
                         .font(.body2Bold())
-                        .foregroundColor(.white)
+                        .foregroundColor(.gray5)
+                    
                 }
-                
-                Text("계속 생성")
-                
+                .frame(width: 280,height: 280)
+                .padding(.vertical, 48)
             }
-            .frame(width: 280,height: 280)
-            .padding(.vertical, 48)
         }
     }
 }
