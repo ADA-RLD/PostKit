422489441ab8dee98e5d14cc3e501b9d673ca636
HIBAR97 <hibar9710@gmail.com>
2023-11-08 17:23:55 +0900

    [Fix] 직접 추가한 변수가 로딩뷰에 나오지 않는 버그 수정

--- PostKit/PostKit/View/Caption/DailyView.swift
+++ PostKit/PostKit/View/Caption/DailyView.swift
@@ -78,7 +78,7 @@ extension DailyView {
                 Task{
                     loadingModel.isCaptionGenerate = false
                     //배열에 추가해서 가져갑니다.
-                    loadingModel.inputArray += weatherSelected + dailyCoffeeSelected + dailyDessertSelected
+                    loadingModel.inputArray = [isSelected, weatherSelected, dailyCoffeeSelected, dailyDessertSelected].flatMap { $0 }
                     
                     DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.8) {
                         sendMessage(weatherSelected: weatherSelected, dailyCoffeeSelected: dailyCoffeeSelected, dailyDessertSelected: dailyDessertSelected, textLength: textLengthArr[textLength])
