de61deb10a5ad7b43200b608e2adf8b74c8f6b06
김다빈 <mokdb17@gmail.com>
2023-11-15 17:24:21 +0900

    [Setting/#260] 파이어 베이스 셋팅 완료

--- PostKit/PostKit/AppDelegate.swift
+++ PostKit/PostKit/AppDelegate.swift
@@ -14,21 +14,25 @@ import Firebase
 class AppDelegate: NSObject, UIApplicationDelegate {
     private let firebaseManager = FirebaseManager()
     private var mixpanelKey: String?
-    // 키 오류를 대비해서 랜덤하게 키를 게속 바꿔줍니다.
+
     func getRandomKey() {
         let chatGptAPIKey = firebaseManager.getDoucument(apiName: "mixpanel") { [weak self] (key) in
             self?.mixpanelKey = key
+            print("앱 시작")
+            print(self?.mixpanelKey ?? "키 값 오류")
+            
+            DispatchQueue.main.async {
+                Mixpanel.initialize(token: self?.mixpanelKey ?? "믹스패널 키 오류", trackAutomaticEvents: true)
+                Mixpanel.mainInstance().track(event: "앱 실행")
+            }
         }
-        print(mixpanelKey ?? "키 값 오류")
     }
-    
-    
-  func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-  ) -> Bool {
-      Mixpanel.initialize(token: mixpanelKey ?? "믹스패널 키 오류", trackAutomaticEvents: true)
-      Mixpanel.mainInstance().track(event: "앱 실행")
-    return true
-  }
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        getRandomKey() // 랜덤 키를 가져오는 함수를 호출해주세요.
+        return true
+    }
 }
--- PostKit/PostKit/ViewModel/ChatGptService.swift
+++ PostKit/PostKit/ViewModel/ChatGptService.swift
@@ -14,38 +14,36 @@ class ChatGptService {
     private let firebaseManager = FirebaseManager()
     private var chatGptKey: String?
     // 키 오류를 대비해서 랜덤하게 키를 게속 바꿔줍니다.
-    func getRandomKey() {
+    
+    func getRandomKey(completion: @escaping () -> Void) {
         let chatGptAPIKey = firebaseManager.getDoucument(apiName: "chatgptAPI") { [weak self] (key) in
             self?.chatGptKey = key
+            completion()
         }
-        print(chatGptKey ?? "없음")
     }
     
     func sendMessage(messages: [Message]) -> AnyPublisher<chatGptResponse, Error> {
-        let openAIMessages = messages.map({chatGptMessage(role: .user, content: $0.content)})
-         getRandomKey()
-        // TODO: - 개발을 진행하는 동안 3.5로 진행하고 이후 배포시 4.0으로 상향 조정할 예정
-        let body = chatGptBody(model: "gpt-4-1106-preview", messages: openAIMessages)
-        let headers: HTTPHeaders =  [
-            "Authorization" : "Bearer \(chatGptKey ?? "키 값 오류")"
-        ]
-        
         return Future <chatGptResponse, Error>{ promise in
-            AF.request(self.baseUrl, method: .post, parameters: body, encoder: .json, headers: headers)
-                .responseDecodable(of: chatGptResponse.self) { response in
-                    switch response.result {
-                    case .success(let result):
-                        print("success: \(result)")
-                        promise(.success(result))
-                    case .failure(let error):
-                        print("error 상세 내용: \(error)")
-                        print("error_code: \(error._code)")
-                        // error code 별 오류
-                        // 10: keyNotFound - choices string not found: nil 값 error
-                        // 13: InternetNotFound - offline인 상태로 인해 error 발생
-                        promise(.failure(error))
+            self.getRandomKey() {
+                let openAIMessages = messages.map({chatGptMessage(role: .user, content: $0.content)})
+                let body = chatGptBody(model: "gpt-4-1106-preview", messages: openAIMessages)
+                let headers: HTTPHeaders =  [
+                    "Authorization" : "Bearer \(self.chatGptKey ?? "키 값 오류")"
+                ]
+                
+                AF.request(self.baseUrl, method: .post, parameters: body, encoder: .json, headers: headers)
+                    .responseDecodable(of: chatGptResponse.self) { response in
+                        switch response.result {
+                        case .success(let result):
+                            print("success: \(result)")
+                            promise(.success(result))
+                        case .failure(let error):
+                            print("error 상세 내용: \(error)")
+                            print("error_code: \(error._code)")
+                            promise(.failure(error))
+                        }
                     }
-                }
+            }
         }
         .eraseToAnyPublisher()
     }
