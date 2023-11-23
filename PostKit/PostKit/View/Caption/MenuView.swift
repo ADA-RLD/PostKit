8df98c4fa61ac02e1200a9e8bf2ce6e473c88cb1
seoyeon-1206 <ssy_1206@naver.com>
2023-11-23 20:47:33 +0900

    [Chore/#279] 정각-> 자정으로 텍스트 수정

--- PostKit/PostKit/View/Caption/MenuView.swift
+++ PostKit/PostKit/View/Caption/MenuView.swift
@@ -60,7 +60,7 @@ struct MenuView: View {
                 hideKeyboard()
             }
             if showAlert {
-                CustomAlertMessage(alertTopTitle: "크레딧을 모두 사용했어요", alertContent: "크레딧이 있어야 생성할 수 있어요\n크레딧은 정각에 충전돼요", topBtnLabel: "확인") {pathManager.path.removeAll()}
+                CustomAlertMessage(alertTopTitle: "크레딧을 모두 사용했어요", alertContent: "크레딧이 있어야 생성할 수 있어요\n크레딧은 자정에 충전돼요", topBtnLabel: "확인") {pathManager.path.removeAll()}
                 }
             }
         .navigationBarBackButtonHidden()
