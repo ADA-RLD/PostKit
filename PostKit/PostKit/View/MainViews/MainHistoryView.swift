534c5692dbba0a757f2ee029dcb9c72a1348a9bc
김다빈 <mokdb17@gmail.com>
2024-01-31 21:14:28 +0900

    [Design/#282] 히스토리뷰 컴포넌트 디자인 변경

--- PostKit/PostKit/View/MainViews/MainHistoryView.swift
+++ PostKit/PostKit/View/MainViews/MainHistoryView.swift
@@ -244,9 +244,16 @@ extension MainHistoryView {
                 HStack(spacing: 8) {
                     Text(date)
                         .body2Bold(textColor: .gray4)
+                    if tag == "일상" || tag == "메뉴" {
+                        Text("카페")
+                            .body2Bold(textColor: .gray4)
+                    }
+                    else {
+                        Text("패션")
+                            .body2Bold(textColor: .gray4)
+                    }
                     
-                    Text(tag)
-                        .body2Bold(textColor: .gray4)
+                   
                 }
                 
                 Text(content.wrappedValue)
