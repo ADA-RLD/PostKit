8086f882741af0fa68a1765d3c260619bda810b7
김다빈 <mokdb17@gmail.com>
2024-02-08 02:48:49 +0900

    [Feat/#287] gpt 변경 완료

--- PostKit/PostKit/ViewModel/DailyViewModel.swift
+++ PostKit/PostKit/ViewModel/DailyViewModel.swift
@@ -47,9 +47,11 @@ extension DailyView {
             toneInfo = toneInfo.substring(from: 0, to: toneInfo.count-2)
         }
         
-        viewModel.basicPrompt = "너는 \(storeModel.storeName)를 운영하고 있으며 \(toneInfo) 말투를 가지고 있어. 그리고 같이 보낸 사진에 대해서도 설명해줘 글은 존댓말로 작성해줘. 다른 부연 설명은 하지 말고 응답 내용만 작성해줘. 글자수는 꼭 \(textLength)자로 맞춰서 작성해줘."
+        viewModel.basicPrompt = "보내준 사진에 대해서 한국말로 설명해줘"
         print(viewModel.basicPrompt)
-        self.messages.append(Message(id: UUID(), role: .system, content: viewModel.basicPrompt))
+        //        self.visionMessages.append(GptVisionMessage( role: .user, content: .array([GptVisionContent(type: "text", text: viewModel.prompt, image_url: nil)])))
+
+        self.visionMessages.append(GptVisionMessage( role: .user, content: .array([GptVisionContent(type: "text", text: viewModel.basicPrompt, image_url: nil)])))
         
         if !weatherSelected.isEmpty {
             pointText = pointText + "오늘 날씨의 특징으로는 "
@@ -95,8 +97,9 @@ extension DailyView {
             pointText = pointText + "이 있어."
         }
         
-        viewModel.prompt = "보내준 사진에 대한 설명과 카페 일상과 관련된 인스타그램 피드를 해시태그 없이 작성해줘.  \(pointText) 글자수는 공백 포함해서 꼭 \(textLength)자로 맞춰서 작성해줘."
-        self.visionMessages.append(GptVisionMessage( role: .user, content: .array([GptVisionContent(type: "text", text: viewModel.prompt, image_url: nil)])))
+//        viewModel.prompt = "보내준 사진에 대한 설명과 카페 일상과 관련된 인스타그램 피드를 해시태그 없이 작성해줘.  \(pointText) 글자수는 공백 포함해서 꼭 500자로 맞춰서 작성해줘."
+        print(viewModel.prompt)
+//        self.visionMessages.append(GptVisionMessage( role: .user, content: .array([GptVisionContent(type: "text", text: viewModel.prompt, image_url: nil)])))
 
    
     }
@@ -170,6 +173,7 @@ extension DailyView {
         }
         
         viewModel.prompt = "카페 일상과 관련된 인스타그램 피드를 해시태그 없이 작성해줘. \(pointText) 글자수는 공백 포함해서 꼭 \(textLength)자로 맞춰서 작성해줘."
+  
         self.messages.append(Message(id: UUID(), role: .user, content: viewModel.prompt))
     }
     
@@ -233,6 +237,7 @@ extension DailyView {
                 },
                 receiveValue: { response in
                     guard let choice = response.choices.first else { return }
+                    print(response)
 
                     // 선택지의 메시지를 가져옵니다.
                     let message = choice.message
@@ -247,6 +252,8 @@ extension DailyView {
 
                     viewModel.category = "일상"
                 }
+                
+                
 
             )
             .store(in: &cancellables)
