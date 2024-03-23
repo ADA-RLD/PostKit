d55e940347ee7c47b43fdb56d3270a90bcba9940
김다빈 <mokdb17@gmail.com>
2024-03-23 17:09:17 +0900

    [Setting/#319] CaptionView, CaptionViewModel, PromptModel 파일 생성

---
+++ PostKit/PostKit/View/Caption/CaptionView.swift
@@ -0,0 +1,18 @@
+//
+//  CaptionView.swift
+//  PostKit
+//
+//  Created by 김다빈 on 3/23/24.
+//
+
+import SwiftUI
+
+struct CaptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CaptionView()
+}
---
+++ PostKit/PostKit/ViewModel/CaptionViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CaptionViewModel.swift
+//  PostKit
+//
+//  Created by 김다빈 on 3/23/24.
+//
+
+import SwiftUI
+
+struct CaptionViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CaptionViewModel()
+}
