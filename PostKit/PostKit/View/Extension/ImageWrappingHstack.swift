4944c20ab2f973b378b535460f51da89e42cc9bc
HIBAR97 <hibar9710@gmail.com>
2024-03-27 20:49:33 +0900

    [Fix/#321] 사진 크기 찌그러짐 수정

--- PostKit/PostKit/View/Extension/ImageWrappingHstack.swift
+++ PostKit/PostKit/View/Extension/ImageWrappingHstack.swift
@@ -9,14 +9,13 @@ import SwiftUI
 
 struct ImageWrappingHstack: View {
     @Binding var ImageData: [UIImage]
-
+    
     var body: some View {
         ScrollView(.horizontal) {
             HStack {
                 ForEach(ImageData.indices, id: \.self) { index in
                     ZStack {
                         Image(uiImage: ImageData[index])
-                            .resizable()
                             .cornerRadius(radius2)
                             .frame(width: 100, height: 100)
 
